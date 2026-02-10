9365666ff3013b37c960fcce13d48b9eba0c4fac
= <=>
2026-02-11 00:02:10 +0100

    add new log

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 
-import SmoothScroll from './components/SmoothScroll';
 import CustomCursor from './components/CustomCursor';
 
 function App() {
     return (
         <Router>
-            <SmoothScroll>
-                <CustomCursor />
-                <Routes>
-                    <Route path="/" element={<LandingPage />} />
-                    {/* <Route path="/product/:id" element={<ProductDetail />} /> */}
-                </Routes>
-            </SmoothScroll>
+            <CustomCursor />
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                {/* <Route path="/product/:id" element={<ProductDetail />} /> */}
+            </Routes>
         </Router>
     );
 }
