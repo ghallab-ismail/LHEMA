23a715edc57071b5546d2ea7d407dbbfd06133e3
= <=>
2026-02-28 00:06:06 +0000

    origin fix

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Security Middlewares
 app.use(helmet());
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'https://lhema.vercel.app', /\.vercel\.app$/],
+    origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'https://lhema.vercel.app', 'https://maisonlhema.com', 'https://www.maisonlhema.com', /\.vercel\.app$/],
     credentials: true,
 }));
 app.set('trust proxy', 1); // Enable trusting the reverse proxy (Vercel) for rate limiting
